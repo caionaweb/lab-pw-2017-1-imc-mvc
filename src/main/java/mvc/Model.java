2ac6f80df156ab70d38589ead766be03a6a64e47
Aluno <Aluno>
2017-06-06 21:43:29 -0300

    Faixas

--- src/main/java/mvc/Model.java
+++ src/main/java/mvc/Model.java
@@ -12,32 +12,44 @@ public class Model {
 
 	public String resultado() {
 		String imcF = String.format("%.2f", imc);
-		
+
 		if (Float.isNaN(imc)) {
 			return "";
 		}
-		
+
 		if (sexo == "f") {
-			if (imc < 19.1) {
-				return "<div class='card-panel red white-text'>IMC: " + imcF
-						+ " <br><b>Cuidado!</b> Você está abaixo do peso.</div>";
-			} else if (imc > 25.8) {
-				return "<div class='card-panel green white-text'>IMC: " + imcF
-						+ " <br><b>Otimo!</b> Você está no peso normal.</div>";
-			} else {
-				return "<div class='card-panel yellow darken-2 white-text'>IMC: " + imcF
-						+ " <br><b>Atenção!</b> Você está marginalmente acima do peso.</div>";
-			}
-		} else {
 			if (imc < 20.7) {
 				return "<div class='card-panel red white-text'>IMC: " + imcF
 						+ " <br><b>Cuidado!</b> Você está abaixo do peso.</div>";
-			} else if (imc > 26.4) {
+			} else if (imc < 26.4) {
 				return "<div class='card-panel green white-text'>IMC: " + imcF
 						+ " <br><b>Otimo!</b> Você está no peso normal.</div>";
-			} else {
+			} else if (imc < 27.8) {
 				return "<div class='card-panel yellow darken-2 white-text'>IMC: " + imcF
-						+ " <br><b>Atenção!</b> Você está marginalmente acima do peso.</div>";
+						+ " <br><b>Atenção!</b> Você marginalmente acima do peso.</div>";
+			} else if (imc < 31.1) {
+				return "<div class='card-panel orange white-text'>IMC: " + imcF
+						+ " <br><b>Cuidado!</b> Você acima do peso ideal.</div>";
+			} else {
+				return "<div class='card-panel red white-text'>IMC: " + imcF
+						+ " <br><b>Cuidado!</b> Você na faixa de obesidade.</div>";
+			}
+		} else {
+			if (imc < 19.1) {
+				return "<div class='card-panel red white-text'>IMC: " + imcF
+						+ " <br><b>Cuidado!</b> Você está abaixo do peso.</div>";
+			} else if (imc < 25.8) {
+				return "<div class='card-panel green white-text'>IMC: " + imcF
+						+ " <br><b>Otimo!</b> Você está no peso normal.</div>";
+			} else if (imc < 27.3) {
+				return "<div class='card-panel yellow darken-2 white-text'>IMC: " + imcF
+						+ " <br><b>Atenção!</b> Você marginalmente acima do peso.</div>";
+			} else if (imc < 32.3) {
+				return "<div class='card-panel orange white-text'>IMC: " + imcF
+						+ " <br><b>Cuidado!</b> Você acima do peso ideal.</div>";
+			} else {
+				return "<div class='card-panel red white-text'>IMC: " + imcF
+						+ " <br><b>Cuidado!</b> Você na faixa de obesidade.</div>";
 			}
 		}
 
